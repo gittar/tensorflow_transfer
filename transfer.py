6f8030c134f73007eea7a97a97b37779ee7f2eef
gittar <fritzke@web.de>
2019-04-05 01:11:01 +0200

    inital

---
+++ transfer.py
@@ -0,0 +1,243 @@
+#@title MIT License
+#
+# Copyright (c) 2017 François Chollet                                                                                                                    # IGNORE_COPYRIGHT: cleared by OSS licensing
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+from __future__ import absolute_import, division, print_function
+
+import os
+
+import tensorflow as tf
+from tensorflow import keras
+print("TensorFlow version is ", tf.__version__)
+
+# filter out warnings
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import time
+
+#
+# get cats and dogs training data
+#
+zip_file = tf.keras.utils.get_file(origin="https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip", 
+                                   fname="cats_and_dogs_filtered.zip", extract=True)
+base_dir, _ = os.path.splitext(zip_file)
+print("Basedir =", base_dir)
+
+#
+# Prepare training and validation datasets
+#
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+
+# Directory with our training cat pictures
+train_cats_dir = os.path.join(train_dir, 'cats')
+print ('Total training cat images:', len(os.listdir(train_cats_dir)))
+
+# Directory with our training dog pictures
+train_dogs_dir = os.path.join(train_dir, 'dogs')
+print ('Total training dog images:', len(os.listdir(train_dogs_dir)))
+
+# Directory with our validation cat pictures
+validation_cats_dir = os.path.join(validation_dir, 'cats')
+print ('Total validation cat images:', len(os.listdir(validation_cats_dir)))
+
+# Directory with our validation dog pictures
+validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+print ('Total validation dog images:', len(os.listdir(validation_dogs_dir)))
+
+g = keras.preprocessing.image.ImageDataGenerator()
+
+#
+# Create Image Data Generator with Image Augmentation
+#
+image_size = 160 # All images will be resized to 160x160
+batch_size = 32
+
+# Rescale all images by 1./255 and apply image augmentation
+train_datagen = keras.preprocessing.image.ImageDataGenerator(
+                rescale=1./255)
+
+validation_datagen = keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+
+# Flow training images in batches of batch_size using train_datagen generator
+train_generator = train_datagen.flow_from_directory(
+                train_dir,  # Source directory for the training images
+                target_size=(image_size, image_size),  
+                batch_size=batch_size,
+                # Since we use binary_crossentropy loss, we need binary labels
+                class_mode='binary')
+
+# Flow validation images in batches of batch_size using test_datagen generator
+validation_generator = validation_datagen.flow_from_directory(
+                validation_dir, # Source directory for the validation images
+                target_size=(image_size, image_size),
+                batch_size=batch_size,
+                class_mode='binary')    
+
+
+#
+# Create the base model from the pre-trained convnets
+#
+
+IMG_SHAPE = (image_size, image_size, 3)
+
+# Create the base model from the pre-trained model MobileNet V2
+base_model = tf.keras.applications.MobileNetV2(input_shape=IMG_SHAPE,
+                                               include_top=False, 
+                                               weights='imagenet')
+
+base_model.trainable = False
+
+base_model.summary()
+
+#
+# add a classification head
+#
+model = tf.keras.Sequential([
+  base_model,
+  keras.layers.GlobalAveragePooling2D(),
+  keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.summary()
+
+print(len(model.trainable_variables),"trainable variables")
+
+model.compile(optimizer=tf.keras.optimizers.RMSprop(lr=0.0001), 
+              loss='binary_crossentropy', 
+              metrics=['accuracy'])
+
+model.summary()
+
+#
+# train
+#
+epochs = 50
+steps_per_epoch = train_generator.n // batch_size
+validation_steps = validation_generator.n // batch_size
+
+start = time.time()
+history = model.fit_generator(train_generator, 
+                              steps_per_epoch = steps_per_epoch,
+                              epochs=epochs, 
+                              workers=4,
+                              validation_data=validation_generator, 
+                              validation_steps=validation_steps)
+end = time.time()
+print("Time used for training: "+str(end-start))
+print(history.history)
+acc = history.history['accuracy'] # was 'acc' in tf 1.x
+val_acc = history.history['val_accuracy'] # was 'val_acc' in tf 1.x
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+plt.figure(figsize=(8, 8))
+plt.subplot(2, 1, 1)
+plt.plot(acc, label='Training Accuracy')
+plt.plot(val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.ylabel('Accuracy')
+plt.ylim([min(plt.ylim()),1])
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(2, 1, 2)
+plt.plot(loss, label='Training Loss')
+plt.plot(val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.ylabel('Cross Entropy')
+plt.ylim([0,max(plt.ylim())])
+plt.title('Training and Validation Loss')
+#plt.show()
+plt.savefig('foo.png')
+
+#
+# Un-freeze the top layers of the modele
+#
+base_model.trainable = True
+
+# Let's take a look to see how many layers are in the base model
+print("Number of layers in the base model: ", len(base_model.layers))
+
+# Fine tune from this layer onwards
+fine_tune_at = 100
+
+# Freeze all the layers before the `fine_tune_at` layer
+for layer in base_model.layers[:fine_tune_at]:
+  layer.trainable =  False
+
+# 
+# Compile the model
+# Compile the model using a much-lower training rate.
+#
+
+model.compile(loss='binary_crossentropy',
+              optimizer = tf.keras.optimizers.RMSprop(lr=2e-5),
+              metrics=['accuracy'])
+
+
+model.summary()
+print(len(model.trainable_variables),"trainable variables")
+
+#
+# Continue training the model
+#
+start = time.time()
+history_fine = model.fit_generator(train_generator, 
+                                   steps_per_epoch = steps_per_epoch,
+                                   epochs=epochs, 
+                                   workers=4,
+                                   validation_data=validation_generator, 
+                                   validation_steps=validation_steps)
+end = time.time()
+print("Time used for fine-training: "+str(end-start))
+
+
+acc += history_fine.history['accuracy']
+val_acc += history_fine.history['val_accuracy']
+
+loss += history_fine.history['loss']
+val_loss += history_fine.history['val_loss']
+
+plt.figure(figsize=(8, 8))
+plt.subplot(2, 1, 1)
+plt.plot(acc, label='Training Accuracy')
+plt.plot(val_acc, label='Validation Accuracy')
+plt.ylim([0.9, 1])
+plt.plot([epochs-1,epochs-1], plt.ylim(), label='Start Fine Tuning')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(2, 1, 2)
+plt.plot(loss, label='Training Loss')
+plt.plot(val_loss, label='Validation Loss')
+plt.ylim([0, 0.2])
+plt.plot([epochs-1,epochs-1], plt.ylim(), label='Start Fine Tuning')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+#plt.show()
+plt.savefig('foo2.png')
+
+
